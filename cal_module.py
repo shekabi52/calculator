b0e885e67da6df048a602d4a6023d863374cf928
python <shekabi52@gmail.com>
2021-07-17 12:03:48 +0530

    first commit

---
+++ cal_module.py
@@ -0,0 +1,25 @@
+class calculator:
+    def add(self,a,b):
+        print(a+b)
+
+    def sub(self,a,b):
+        print(a-b)
+
+    def mul(self,a,b):
+        print(a*b)
+
+    def div(self,a,b):
+        print(a//b)
+
+    def expo(self,a,b):
+        print(a**b)
+
+class real:
+    def main(self):
+        cal=calculator()
+
+
+
+if __name__=="__main__":
+    r=real()
+    r.main()
---
+++ cal_user.py
@@ -0,0 +1,26 @@
+import cal_module
+
+c=cal_module.calculator()
+
+def fun(i):
+    if(i==1):
+        c.add(a,b)
+    elif(i==2):
+        c.sub(a,b)
+    elif (i == 3):
+        c.mul(a, b)
+    elif (i == 4):
+        c.div(a, b)
+    elif (i == 5):
+        c.expo(a, b)
+    else:
+        return 0
+    return 1
+
+ret=0
+while(ret!=1):
+    x = int(input("Enter an option "))
+    if(x<=5):
+        a = int(input("enter num1 "))
+        b = int(input("enter num2 "))
+    ret = fun(x)
